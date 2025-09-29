97e48cc61428a5357032b818a6ea3f5d27df82b9
Vibhanshu Prasad <vibhanshuprasad@gmail.com>
2025-09-29 13:19:37 +0530

    test haskell

--- haskell/hello.hs
+++ haskell/hello.hs
@@ -18,6 +18,25 @@ csignum n
       | n == 0    = 0
       | otherwise = 1
 
+cons :: a -> (b -> a)
+cons x = \_ -> x
+
+factors :: Int -> [Int]
+factors n = [x| x <- [1..n], n `mod` x == 0]
+
+factorial1 :: Int -> Int
+factorial1 n = product [1..n]
+
+factorial2 :: Integer -> Integer
+factorial2 0 = 1
+factorial2 n  = n * (factorial2 (n - 1))
+
+insert :: Ord a => a -> [a] -> [a]
+insert x []  =   [x]
+insert x (y:ys)   |   x <= y        = x:y:ys
+                  |  otherwise      = y:insert x ys
+
 main :: IO ()
 main = putStrLn "Hello, World"
+
 
